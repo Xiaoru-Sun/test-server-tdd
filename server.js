c0d2286cab10dfa6558be36dacafe8c8128ddb59
Xiaoru Sun <xiaorusun@Xiaorus-MacBook-Air.local>
2024-05-22 09:24:18 +0100

    adds crud controllers & models

--- server.js
+++ server.js
@@ -11,66 +11,6 @@ const uri =
 
 const client = new MongoClient(uri);
 
-async function main() {
-  const uri =
-    "mongodb+srv://xrssun:3W0uGdjh5gec8dKX@cluster0.z60yugb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
-  const client = new MongoClient(uri);
-  try {
-    await client.connect();
-    console.log("database connected");
-    // const fetched = await getProducts(client);
-    // return fetched;
-
-    // await insertExercise(client, {
-    //   exercise_id: 2,
-    //   exercise_name: "waling",
-    //   distance: 0,
-    // });
-
-    // await insertActivity(client, [
-    //   { activity_id: 1, exercise_name: "running", user_id: 3, distance: null },
-    // ]);
-  } catch (e) {
-    console.error(e);
-  } finally {
-    await client.close();
-  }
-}
-
-// main().catch(console.error);
-
-async function getProducts(client) {
-  const result = await client
-    .db("store")
-    .collection("products")
-    .find({})
-    .toArray();
-  return result;
-}
-
-async function insertExercise(client, newExercise) {
-  const result = await client
-    .db("trailblaze")
-    .collection("exercise")
-    .insertOne(newExercise);
-  console.log(`Exercise added ${result.insertedId}`);
-}
-
-async function insertActivity(client, newActivities) {
-  const result = await client
-    .db("trailblaze")
-    .collection("activity")
-    .insertMany(newActivities);
-
-  console.log(`${result.insertedCount} new activities added `);
-  console.log(`${result.insertedIds}`);
-}
-
-async function getAllUsers(client) {
-  const result = await client.db("trailblaze").collection("users").toArray();
-}
-
 app.get("/", async (req, res) => {
   // const tosend = await main();
   // res.send({ tosend });
@@ -99,3 +39,63 @@ app.get("/users/:user_id", async (req, res) => {
 app.post("/users/");
 
 app.listen(9090, () => console.log("App listening on port 9090!"));
+
+// async function main() {
+//   const uri =
+//     "mongodb+srv://xrssun:3W0uGdjh5gec8dKX@cluster0.z60yugb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+//   const client = new MongoClient(uri);
+//   try {
+//     await client.connect();
+//     console.log("database connected");
+//     // const fetched = await getProducts(client);
+//     // return fetched;
+
+//     // await insertExercise(client, {
+//     //   exercise_id: 2,
+//     //   exercise_name: "waling",
+//     //   distance: 0,
+//     // });
+
+//     // await insertActivity(client, [
+//     //   { activity_id: 1, exercise_name: "running", user_id: 3, distance: null },
+//     // ]);
+//   } catch (e) {
+//     console.error(e);
+//   } finally {
+//     await client.close();
+//   }
+// }
+
+// // main().catch(console.error);
+
+// async function getProducts(client) {
+//   const result = await client
+//     .db("store")
+//     .collection("products")
+//     .find({})
+//     .toArray();
+//   return result;
+// }
+
+// async function insertExercise(client, newExercise) {
+//   const result = await client
+//     .db("trailblaze")
+//     .collection("exercise")
+//     .insertOne(newExercise);
+//   console.log(`Exercise added ${result.insertedId}`);
+// }
+
+// async function insertActivity(client, newActivities) {
+//   const result = await client
+//     .db("trailblaze")
+//     .collection("activity")
+//     .insertMany(newActivities);
+
+//   console.log(`${result.insertedCount} new activities added `);
+//   console.log(`${result.insertedIds}`);
+// }
+
+async function getAllUsers(client) {
+  const result = await client.db("trailblaze").collection("users").toArray();
+}
